0631f4a9423761584af871e09669001b8d2e3c68
Owais28 <owaisathar2882@gmail.com>
2022-05-12 17:17:15 +0530

    jdd

--- src/App.js
+++ src/App.js
@@ -2,7 +2,28 @@
 import Container from './styles/Container.styled'
 import Section from './styles/Sections.styled'
 import SearchBar from './components/SearchBar'
+import React from 'react'
+
+// d500b261
+
+const API_URL = 'http://www.omdbapi.com/?apikey=d500b261'
+
+const searchMovie = async (title='spiderman') => {
+  const response = await fetch(`${API_URL}&s=${title}`);
+  const data = await response.json()
+  console.log(data.Search[0])
+
+}
+
+
 function App() {
+
+  React.useEffect(
+    () => {
+      searchMovie();
+    }
+  ,[])
+
   return (
     <Section>
       <Container>
