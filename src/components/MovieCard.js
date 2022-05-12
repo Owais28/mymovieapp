d7f7c886f84b7275a42c051049f02850176b7ee6
Owais28 <owaisathar2882@gmail.com>
2022-05-12 20:16:46 +0530

    fsffdsd

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Card } from '../styles/Cards.styled'
-import { GridOfTwo } from '../styles/Grids.styled';
-import {HeadingOne, HeadingThree, HeadingTwo} from '../styles/Headings.styled'
+// import { GridOfTwo } from '../styles/Grids.styled';
+import {HeadingThree} from '../styles/Headings.styled'
 export default function MovieCard(props) {
     const title = props.Title || 'none';
     const img = props.Poster || 'https://bitsofco.de/content/images/2018/12/Screenshot-2018-12-16-at-21.06.29.png';
